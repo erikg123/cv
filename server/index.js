de24a221cf03af17dc3cca541ddbe1e530cc0ac7
Erik Granbom <ergra1@mma-c02w426uhv2h.global.ad>
2018-04-04 11:48:09 +0200

    chore(): initial setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const rootValue = { hello: () => 'Hello world!' };
+
+const app = express();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+  })
+);
+app.listen(4000);
